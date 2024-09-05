484ec1785540f7e6312e908eaaa2894eaaadb02b
KronosLeosT* <165606409+Kronos27171@users.noreply.github.com>
2024-09-05 22:36:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -54,10 +54,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/9d43720a49a6a9baa885f.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
