c20cdeb430a21c695d1269118e09e40c19868e58
KronosLeosT* <165606409+Kronos27171@users.noreply.github.com>
2024-09-05 22:04:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", "20416838"))
 API_HASH = getenv("API_HASH", "c393e906f7d2610ad5322af34ee4197d")
-BOT_TOKEN = getenv("BOT_TOKEN", "7176732389:AAFplpjlMzT8n71m5zYRFTdaNPaTFck8yY0")
+BOT_TOKEN = getenv("BOT_TOKEN", "6625853323:AAFUsBRZZPOIJH5lOc4SJQFgYyto_ndovKM")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://hakoqq27:Hakan2717@cluster0.qpyasii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 500))
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002111165628"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002140930364"))
 OWNER_ID = int(getenv("OWNER_ID", 6939182031))
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
