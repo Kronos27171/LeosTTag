8e5b9306fb76dd81c0ebc2d1ac877559a3bb8847
KronosLeosT* <165606409+Kronos27171@users.noreply.github.com>
2024-09-06 19:56:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", "20416838"))
 API_HASH = getenv("API_HASH", "c393e906f7d2610ad5322af34ee4197d")
-BOT_TOKEN = getenv("BOT_TOKEN", "7547386899:AAGkkGPRwS8UMyRoiWUI7mMNuIHgFPKDudQ")
+BOT_TOKEN = getenv("BOT_TOKEN", "7547386899:AAG0X5On0pSq8HBFoq6cRko7NsBdYp1aN2g")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://hakoqq27:Hakan2717@cluster0.qpyasii.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 500))
 LOGGER_ID = int(getenv("LOGGER_ID", "-1002140930364"))
